6f32506c10b98f96e3c87735712cba861e131a49
poteto <naonyan1135@gmail.com>
2024-04-02 23:46:02 +0900

    楽しいruby楽しい 文字多いけど読みやすい

---
+++ while.rb
@@ -0,0 +1,14 @@
+# while文
+i = 1
+while i <= 10
+  print i, "\n"
+  i += 1  # iを1増やす
+end
+ # timeメゾット
+ 100.times do
+  print "All work and no play makes Jack a dull boy.\n"
+ end
+
+=begin
+timesメゾットは**イテレータ**であり、「繰り返すもの」という意味です
+=end
